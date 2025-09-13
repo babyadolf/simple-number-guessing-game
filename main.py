4d607680ba5f216a05258178cbf58fd749cb779b
Keandrian <biipol4r@proton.me>
2025-09-13 23:23:52 +0200

    Made the script

---
+++ main.py
@@ -0,0 +1,16 @@
+# Simple number guessing game
+
+import random
+
+number = random.randint(1, 10)
+guess = 0
+
+while guess != number:
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print("Congratulations! You've guessed the number.")
